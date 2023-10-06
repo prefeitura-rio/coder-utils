e8e4e3d8af59e29228edf5e8f550d17bbdc58898
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2023-10-06 08:19:23 -0300

    feat: start postgres

--- templates/kubernetes/main.tf
+++ templates/kubernetes/main.tf
@@ -115,6 +115,8 @@ resource "coder_agent" "main" {
     # install and start code-server
     curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.8.3
     /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
+    # start postgresql in the background
+    sudo start-stop-daemon --start --chuid "postgres:postgres" --exec "/usr/lib/postgresql/12/bin/postgres" -- --config-file=/etc/postgresql/12/main/postgresql.conf >/tmp/postgresql.log 2>&1 &
   EOT
 }
 
